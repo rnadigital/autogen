49a8b8fac389d28ca882fdb9a007083f82222e94
ragy abraham <ragy@rna.digital>
2023-11-07 15:40:42 +1100

    wrapping execute_code function in a try-catch. Adding docker to the requirements

--- autogen/code_utils.py
+++ autogen/code_utils.py
@@ -9,6 +9,7 @@ from hashlib import md5
 import logging
 from autogen import oai
 from concurrent.futures import ThreadPoolExecutor, TimeoutError
+from docker.errors import ImageNotFound
 
 try:
     import docker
@@ -252,153 +253,158 @@ def execute_code(
         str: The error message if the code fails to execute; the stdout otherwise.
         image: The docker image name after container run when docker is used.
     """
-    if all((code is None, filename is None)):
-        error_msg = f"Either {code=} or {filename=} must be provided."
-        logger.error(error_msg)
-        raise AssertionError(error_msg)
+    try:
+        if all((code is None, filename is None)):
+            error_msg = f"Either {code=} or {filename=} must be provided."
+            logger.error(error_msg)
+            raise AssertionError(error_msg)
 
-    # Warn if docker was requested but cannot be provided. In this case
-    # the current behavior is to fall back to run natively, but this behavior
-    # is subject to change.
-    if use_docker and docker is None:
-        use_docker = False
-        logger.warning(
-            "execute_code was called with use_docker evaluating to True, but the python docker package is not available. Falling back to native code execution. Note: this fallback behavior is subject to change"
-        )
-
-    timeout = timeout or DEFAULT_TIMEOUT
-    original_filename = filename
-    if WIN32 and lang in ["sh", "shell"] and (not use_docker):
-        lang = "ps1"
-    if filename is None:
-        code_hash = md5(code.encode()).hexdigest()
-        # create a file with a automatically generated name
-        filename = f"tmp_code_{code_hash}.{'py' if lang.startswith('python') else lang}"
-    if work_dir is None:
-        work_dir = WORKING_DIR
-    filepath = os.path.join(work_dir, filename)
-    file_dir = os.path.dirname(filepath)
-    os.makedirs(file_dir, exist_ok=True)
-    if code is not None:
-        with open(filepath, "w", encoding="utf-8") as fout:
-            fout.write(code)
-    # check if already running in a docker container
-    in_docker_container = os.path.exists("/.dockerenv")
-    if not use_docker or in_docker_container:
-        # already running in a docker container
-        cmd = [
-            sys.executable if lang.startswith("python") else _cmd(lang),
-            f".\\{filename}" if WIN32 else filename,
-        ]
-        if WIN32:
-            logger.warning("SIGALRM is not supported on Windows. No timeout will be enforced.")
-            result = subprocess.run(
-                cmd,
-                cwd=work_dir,
-                capture_output=True,
-                text=True,
+        # Warn if docker was requested but cannot be provided. In this case
+        # the current behavior is to fall back to run natively, but this behavior
+        # is subject to change.
+        if use_docker and docker is None:
+            use_docker = False
+            logger.warning(
+                "execute_code was called with use_docker evaluating to True, but the python docker package is not available. Falling back to native code execution. Note: this fallback behavior is subject to change"
             )
-        else:
-            with ThreadPoolExecutor(max_workers=1) as executor:
-                future = executor.submit(
-                    subprocess.run,
+
+        timeout = timeout or DEFAULT_TIMEOUT
+        original_filename = filename
+        if WIN32 and lang in ["sh", "shell"] and (not use_docker):
+            lang = "ps1"
+        if filename is None:
+            code_hash = md5(code.encode()).hexdigest()
+            # create a file with a automatically generated name
+            filename = f"tmp_code_{code_hash}.{'py' if lang.startswith('python') else lang}"
+        if work_dir is None:
+            work_dir = WORKING_DIR
+        filepath = os.path.join(work_dir, filename)
+        file_dir = os.path.dirname(filepath)
+        os.makedirs(file_dir, exist_ok=True)
+        if code is not None:
+            with open(filepath, "w", encoding="utf-8") as fout:
+                fout.write(code)
+        # check if already running in a docker container
+        in_docker_container = os.path.exists("/.dockerenv")
+        if not use_docker or in_docker_container:
+            # already running in a docker container
+            cmd = [
+                sys.executable if lang.startswith("python") else _cmd(lang),
+                f".\\{filename}" if WIN32 else filename,
+            ]
+            if WIN32:
+                logger.warning("SIGALRM is not supported on Windows. No timeout will be enforced.")
+                result = subprocess.run(
                     cmd,
                     cwd=work_dir,
                     capture_output=True,
                     text=True,
                 )
-                try:
-                    result = future.result(timeout=timeout)
-                except TimeoutError:
-                    if original_filename is None:
-                        os.remove(filepath)
-                    return 1, TIMEOUT_MSG, None
-        if original_filename is None:
-            os.remove(filepath)
-        if result.returncode:
-            logs = result.stderr
-            if original_filename is None:
-                abs_path = str(pathlib.Path(filepath).absolute())
-                logs = logs.replace(str(abs_path), "").replace(filename, "")
             else:
-                abs_path = str(pathlib.Path(work_dir).absolute()) + PATH_SEPARATOR
-                logs = logs.replace(str(abs_path), "")
-        else:
-            logs = result.stdout
-        return result.returncode, logs, None
+                with ThreadPoolExecutor(max_workers=1) as executor:
+                    future = executor.submit(
+                        subprocess.run,
+                        cmd,
+                        cwd=work_dir,
+                        capture_output=True,
+                        text=True,
+                    )
+                    try:
+                        result = future.result(timeout=timeout)
+                    except TimeoutError:
+                        if original_filename is None:
+                            os.remove(filepath)
+                        return 1, TIMEOUT_MSG, None
+            if original_filename is None:
+                os.remove(filepath)
+            if result.returncode:
+                logs = result.stderr
+                if original_filename is None:
+                    abs_path = str(pathlib.Path(filepath).absolute())
+                    logs = logs.replace(str(abs_path), "").replace(filename, "")
+                else:
+                    abs_path = str(pathlib.Path(work_dir).absolute()) + PATH_SEPARATOR
+                    logs = logs.replace(str(abs_path), "")
+            else:
+                logs = result.stdout
+            return result.returncode, logs, None
 
-    # create a docker client
-    client = docker.from_env()
-    image_list = (
-        ["python:3-alpine", "python:3", "python:3-windowsservercore"]
-        if use_docker is True
-        else [use_docker]
-        if isinstance(use_docker, str)
-        else use_docker
-    )
-    for image in image_list:
-        # check if the image exists
-        try:
-            client.images.get(image)
-            break
-        except docker.errors.ImageNotFound:
-            # pull the image
-            print("Pulling image", image)
+        # create a docker client
+        client = docker.from_env()
+        image_list = (
+            ["python:3-alpine", "python:3", "python:3-windowsservercore"]
+            if use_docker is True
+            else [use_docker]
+            if isinstance(use_docker, str)
+            else use_docker
+        )
+        for image in image_list:
+            # check if the image exists
             try:
-                client.images.pull(image)
+                client.images.get(image)
                 break
-            except docker.errors.DockerException:
-                print("Failed to pull image", image)
-    # get a randomized str based on current time to wrap the exit code
-    exit_code_str = f"exitcode{time.time()}"
-    abs_path = pathlib.Path(work_dir).absolute()
-    cmd = [
-        "sh",
-        "-c",
-        f"{_cmd(lang)} {filename}; exit_code=$?; echo -n {exit_code_str}; echo -n $exit_code; echo {exit_code_str}",
-    ]
-    # create a docker container
-    container = client.containers.run(
-        image,
-        command=cmd,
-        working_dir="/workspace",
-        detach=True,
-        # get absolute path to the working directory
-        volumes={abs_path: {"bind": "/workspace", "mode": "rw"}},
-    )
-    start_time = time.time()
-    while container.status != "exited" and time.time() - start_time < timeout:
-        # Reload the container object
-        container.reload()
-    if container.status != "exited":
-        container.stop()
+            except docker.errors.ImageNotFound:
+                # pull the image
+                print("Pulling image", image)
+                try:
+                    client.images.pull(image)
+                    break
+                except docker.errors.DockerException:
+                    print("Failed to pull image", image)
+        # get a randomized str based on current time to wrap the exit code
+        exit_code_str = f"exitcode{time.time()}"
+        abs_path = pathlib.Path(work_dir).absolute()
+        cmd = [
+            "sh",
+            "-c",
+            f"{_cmd(lang)} {filename}; exit_code=$?; echo -n {exit_code_str}; echo -n $exit_code; echo {exit_code_str}",
+        ]
+        # create a docker container
+        container = client.containers.run(
+            image,
+            command=cmd,
+            working_dir="/workspace",
+            detach=True,
+            # get absolute path to the working directory
+            volumes={abs_path: {"bind": "/workspace", "mode": "rw"}},
+        )
+        start_time = time.time()
+        while container.status != "exited" and time.time() - start_time < timeout:
+            # Reload the container object
+            container.reload()
+        if container.status != "exited":
+            container.stop()
+            container.remove()
+            if original_filename is None:
+                os.remove(filepath)
+            return 1, TIMEOUT_MSG, image
+        # get the container logs
+        logs = container.logs().decode("utf-8").rstrip()
+        # commit the image
+        tag = filename.replace("/", "")
+        container.commit(repository="python", tag=tag)
+        # remove the container
         container.remove()
+        # check if the code executed successfully
+        exit_code = container.attrs["State"]["ExitCode"]
+        if exit_code == 0:
+            # extract the exit code from the logs
+            pattern = re.compile(f"{exit_code_str}(\\d+){exit_code_str}")
+            match = pattern.search(logs)
+            exit_code = 1 if match is None else int(match.group(1))
+            # remove the exit code from the logs
+            logs = logs if match is None else pattern.sub("", logs)
+
         if original_filename is None:
             os.remove(filepath)
-        return 1, TIMEOUT_MSG, image
-    # get the container logs
-    logs = container.logs().decode("utf-8").rstrip()
-    # commit the image
-    tag = filename.replace("/", "")
-    container.commit(repository="python", tag=tag)
-    # remove the container
-    container.remove()
-    # check if the code executed successfully
-    exit_code = container.attrs["State"]["ExitCode"]
-    if exit_code == 0:
-        # extract the exit code from the logs
-        pattern = re.compile(f"{exit_code_str}(\\d+){exit_code_str}")
-        match = pattern.search(logs)
-        exit_code = 1 if match is None else int(match.group(1))
-        # remove the exit code from the logs
-        logs = logs if match is None else pattern.sub("", logs)
-
-    if original_filename is None:
-        os.remove(filepath)
-    if exit_code:
-        logs = logs.replace(f"/workspace/{filename if original_filename is None else ''}", "")
-    # return the exit code, logs and image
-    return exit_code, logs, f"python:{tag}"
+        if exit_code:
+            logs = logs.replace(f"/workspace/{filename if original_filename is None else ''}", "")
+        # return the exit code, logs and image
+        return exit_code, logs, f"python:{tag}"
+    except Exception as e:
+        logging.warning("An error occurred while executing code block")
+        logging.exception(e)
+        return 1, e.args[0], use_docker
 
 
 _GENERATE_ASSERTIONS_CONFIG = {
