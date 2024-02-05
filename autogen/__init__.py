947c9e6dd67405c03b5834f3d7e8ce099acbd224
Joshua Kim <joshua.kim@collagis.com.au>
2024-02-05 14:30:54 +1100

    Expose LlavaAgent

--- autogen/__init__.py
+++ autogen/__init__.py
@@ -4,6 +4,9 @@ from .oai import *
 from .agentchat import *
 from .code_utils import DEFAULT_MODEL, FAST_MODEL
 
+# AgentCloud Exposed LLavaAgent
+from .agentchat.contrib.llava_agent import LlavaAgent
+
 
 # Set the root logger.
 logger = logging.getLogger(__name__)
