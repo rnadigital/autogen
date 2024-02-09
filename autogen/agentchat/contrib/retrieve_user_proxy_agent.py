7471b07b3141469a9324e8c3eb1cb36848d7bab8
pebeid <48051935+pebeid@users.noreply.github.com>
2024-02-09 17:39:59 +1100

    Implement more user friendly no relevant context message

--- autogen/agentchat/contrib/retrieve_user_proxy_agent.py
+++ autogen/agentchat/contrib/retrieve_user_proxy_agent.py
@@ -35,7 +35,7 @@ context provided by the user. You should follow the following steps to answer a 
 Step 1, you estimate the user's intent based on the question and context. The intent can be a code generation task or
 a question answering task.
 Step 2, you reply based on the intent.
-If you can't answer the question with or without the current context, you should reply exactly `UPDATE CONTEXT`.
+If you can't answer the question with or without the current context, you should reply exactly `Answer cannot be ascertained because not enough relevant information is provided in the document`.
 If user's intent is code generation, you must obey the following rules:
 Rule 1. You MUST NOT install any packages because all the packages needed are already installed.
 Rule 2. You must follow the formats below to write your code:
@@ -52,7 +52,7 @@ Context is: {input_context}
 
 PROMPT_CODE = """You're a retrieve augmented coding assistant. You answer user's questions based on your own knowledge and the
 context provided by the user.
-If you can't answer the question with or without the current context, you should reply exactly `UPDATE CONTEXT`.
+If you can't answer the question with or without the current context, you should reply exactly `Answer cannot be ascertained because not enough relevant information is provided in the document`.
 For code generation, you must obey the following rules:
 Rule 1. You MUST NOT install any packages because all the packages needed are already installed.
 Rule 2. You must follow the formats below to write your code:
@@ -67,7 +67,7 @@ Context is: {input_context}
 
 PROMPT_QA = """You're a retrieve augmented chatbot. You answer user's questions based on your own knowledge and the
 context provided by the user.
-If you can't answer the question with or without the current context, you should reply exactly `UPDATE CONTEXT`.
+If you can't answer the question with or without the current context, you should reply exactly `Answer cannot be ascertained because not enough relevant information is provided in the document`.
 You must give as short an answer as possible.
 
 User's question is: {input_question}
